b285dc2840bc2ded8a597fa5ee65f7ed9951a362
Samuel Gélineau <gelisam+github@gmail.com>
2020-04-22 13:28:34 -0400

    test if the linker is thread-safe

--- unit-tests/run-unit-tests.hs
+++ unit-tests/run-unit-tests.hs
@@ -5,10 +5,11 @@ import Prelude hiding (catch)
 import Control.Exception.Extensible (ArithException(..), AsyncException(UserInterrupt))
 import Control.Monad.Catch as MC
 
-import Control.Monad (liftM, when, void, (>=>))
+import Control.Monad (guard, liftM, when, void, (>=>))
 
 import Control.Concurrent (forkIO, threadDelay)
 import Control.Concurrent.MVar
+import Control.Concurrent.STM
 
 import Data.IORef
 import Data.Proxy
@@ -22,7 +23,7 @@ import System.Exit
 import System.Posix.Signals
 #endif
 
-import Test.HUnit ((@?=), (@?))
+import Test.HUnit ((@?=), (@?), assertFailure)
 import qualified Test.HUnit as HUnit
 
 import Language.Haskell.Interpreter
@@ -206,6 +207,12 @@ test_catch = TestCase "catch" [] $ do
                       return $! s
 
 #ifndef THREAD_SAFE_LINKER
+-- Prior to ghc-8.10, the linker wasn't thread-safe, and so running multiple
+-- instances of hint in different threads can lead to mysterious errors of the
+-- form "Could not load '*_closure', dependency unresolved". To make that error
+-- less mysterious, 'ifInterpreterNotRunning' throw a clearer error earlier, as
+-- soon as it detects that the user is trying to run multiple instances of hint
+-- in parallel. This test ensures that this nicer error is thrown.
 test_only_one_instance :: TestCase
 test_only_one_instance = TestCase "only_one_instance" [] $ liftIO $ do
         r <- newEmptyMVar
@@ -216,15 +223,57 @@ test_only_one_instance = TestCase "only_one_instance" [] $ liftIO $ do
         _ <- forkIO $ Control.Monad.void concurrent
         readMVar r @?  "concurrent instance did not fail"
 #else
+-- Prior to ghc-8.10, the linker wasn't thread-safe, and so running multiple
+-- instances of hint in different threads can lead to mysterious errors of the
+-- form "Could not load '*_closure', dependency unresolved". This test ensures
+-- that this error no longer occurs. The important thing about this test is
+-- that it should _fail_ if 'ifInterpreterNotRunning' is deleted and this test
+-- is run on an older ghc version. Otherwise this test is not testing what it's
+-- meant to.
 test_multiple_instances :: TestCase
-test_multiple_instances = TestCase "multiple_instances" [] $ liftIO $ do
-        r <- newEmptyMVar
-        let concurrent = runInterpreter (liftIO $ putMVar r True)
-                          `catch` \MultipleInstancesNotAllowed ->
-                                    do liftIO $ putMVar r False
-                                       return $ Right ()
-        _ <- forkIO $ Control.Monad.void concurrent
-        readMVar r @?  "failed on concurrent instances"
+test_multiple_instances = TestCase "multiple_instances" ["mod_file"] $ liftIO $ do
+        writeFile mod_file "f = id"
+
+        -- ensure the two threads interleave in a deterministic way
+        tvar <- newTVarIO 1
+        let step n = liftIO $ atomically $ do
+              nextStep <- readTVar tvar
+              guard (nextStep >= n)
+              writeTVar tvar (n + 1)
+            skipToStep n = liftIO $ atomically $ do
+              modifyTVar tvar (max n)
+
+        mvar1 <- newEmptyMVar
+        mvar2 <- newEmptyMVar
+        void $ forkIO $ do
+          r1 <- try $ runInterpreter $ do
+            step 1
+            loadModules [mod_file]
+            step 3
+            setTopLevelModules ["Main"]
+            step 5
+            setImports ["Prelude"]
+            step 7
+            eval "f [1,2]" @@?= "[1,2]"
+            step 9
+          skipToStep 9
+          putMVar mvar1 r1
+        void $ forkIO $ do
+          r2 <- try $ runInterpreter $ do
+            step 2
+            loadModules [mod_file]
+            step 4
+            setTopLevelModules ["Main"]
+            step 6
+            setImports ["Prelude"]
+            step 8
+            eval "f [1,2]" @@?= "[1,2]"
+            step 10
+          skipToStep 10
+          putMVar mvar2 r2
+        noInterpreterError =<< noExceptions =<< takeMVar mvar1
+        noInterpreterError =<< noExceptions =<< takeMVar mvar2
+    where mod_file = "TEST_MultipleInstances.hs"
 #endif
 
 test_normalize_type :: TestCase
@@ -347,6 +396,14 @@ fails action = (action >> return False) `catchIE` (\_ -> return True)
 succeeds :: (MonadCatch m, MonadIO m) => m a -> m Bool
 succeeds = fmap not . fails
 
+noExceptions :: Either SomeException a -> IO a
+noExceptions (Left  e) = assertFailure (show e)
+noExceptions (Right a) = pure a
+
+noInterpreterError :: Either InterpreterError a -> IO a
+noInterpreterError (Left  e) = assertFailure (show e)
+noInterpreterError (Right a) = pure a
+
 data IOTestCase = IOTestCase String [FilePath] ((Interpreter () -> IO (Either InterpreterError ())) -> IO (Either InterpreterError ()))
 
 runIOTests :: Bool -> [IOTestCase] -> IO HUnit.Counts
@@ -357,8 +414,7 @@ runIOTests sandboxed = HUnit.runTestTT . HUnit.TestList . map build
                   clean_up = mapM_ removeIfExists tmps
                   go       = do r <- test (\body -> runInterpreter
                                             (when sandboxed setSandbox >> body))
-                                either (printInterpreterError >=> (fail . show))
-                                       return r
+                                noInterpreterError r
                   removeIfExists f = do existsF <- doesFileExist f
                                         if existsF
                                           then removeFile f
